77b59430b8208889971cfdc52ed4e404765b649a
Beeemba <Streletsvlad@yandex.ru>
2026-09-16 14:46:48 +0500

    feat: добавлена функция getElement с проверкой границ
    - возврат ссылки (int&) для использования слева от =
    - static dummy как заглушка при выходе за границы
    - проверка index < 0 || index >= arr.size
    - вывод ошибки с указанием индекса и размера
    - демонстрация getElement(myArr, 2) = 999

--- 1_3_laba.cpp
+++ 1_3_laba.cpp
@@ -14,9 +14,30 @@ SafeArray createArray(int size)
     return arr;
 }
 
+int& getElement(SafeArray& arr, int index)
+{
+static int a = 0;
+if (index < 0 || index >= arr.size)
+{
+    std::cout << "Ошибка: индекс " << index << " выходит за границы массива (размер: " << arr.size << ")\n";
+    return a;
+}
+return arr.data[index];
+}
+
 int main()
 {
 SafeArray Arr = createArray(5);
 std::cout << "Создан массив размером 5\n";
+std::cout <<"Заполнение массива через getElement:\n";
+for (int i = 0; i < Arr.size; i++)
+{
+    getElement(Arr, i) = (i + 1) * 50;
+}
+std::cout << " \ngetElement(Arr, 2) = 999:\n";
+getElement(Arr, 2) = 999;
+std::cout << "\nПопытка доступа за границами (getElement(Arr,20)):\n";
+int& ref = getElement(Arr, 20);
+std::cout << "Возвращена ссылка на заглушку: " << ref << "\n";
 return 0;
 }
